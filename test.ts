83110a2cccccb7b2fe533c2e49560d08c1b23a28
elsitlte <1570056132@qq.com>
2018-09-03 22:46:49 +0800

    fix bug

--- test.ts
+++ test.ts
@@ -1,17 +1,17 @@
 basic.showIcon(IconNames.Heart)
 
 basic.forever(function () {
-    serial.writeValue("light", magibit.lightSensorReadValue(magibit.LightSensorPins.P0))
+    serial.writeValue("light", magibit.LightSensorReadValue(magibit.LightSensorPins.P0))
     serial.writeValue("infrared", magibit.MotorSpeedSensorReadValue(magibit.InfraredSensorPins.P13))
     basic.pause(100)
-    magibit.ledSetBrightness(magibit.LEDPin.P0, 500)
+    magibit.LedSetBrightness(magibit.LEDPin.P0, 500)
     basic.pause(1500)
-    magibit.ledSetOnOff(magibit.LEDPin.P0, magibit.LEDState.OFF)
+    magibit.LedSetOnOff(magibit.LEDPin.P0, magibit.LEDState.OFF)
     basic.pause(1500)
-    magibit.motorSetSpeed(magibit.Motor.M1, magibit.MotorDirection.Forward, 500)
-    magibit.motorSetSpeed(magibit.Motor.M2, magibit.MotorDirection.Forward, 500)
+    magibit.MotorSetSpeed(magibit.Motor.M1, magibit.MotorDirection.Forward, 500)
+    magibit.MotorSetSpeed(magibit.Motor.M2, magibit.MotorDirection.Forward, 500)
     basic.pause(1500)
-    magibit.motorStopAll()
+    magibit.MotorStopAll()
     basic.pause(1500)
 })
 
